a8163b528ccb6d4615634bbc20a02438e60875cc
tito <jfctito@gmail.com>
2024-05-22 16:27:04 +0200

    Automovil Class getters y setters

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,10 +1,50 @@
 public class Automovil {
 
-    String fabricante;
-    String modelo;
-    String color;
-    double cilindrada;
-    int capacidadGasolina = 40;
+    private String fabricante;
+    private String modelo;
+    private String color;
+    private double cilindrada;
+    private int capacidadGasolina = 40;
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCilindrada(double cilindrada){
+        this.cilindrada = cilindrada;
+    }
+
+    public void setCapacidadGasolina(int capacidadGasolina) {
+        this.capacidadGasolina = capacidadGasolina;
+    }
+
+    public String getFabricante(){
+        return this.fabricante;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public double getCilindrada(){
+        return this.cilindrada;
+    }
+
+    public int getCapacidadGasolina(){
+        return this.capacidadGasolina;
+    }
 
     public String verDetalle(){
         return "auto.fabricante = " + this.fabricante + "\nauto.modelo = " + this.modelo + "\nauto.colo = " + this.color + "\naunto.cilindrada = " + this.cilindrada;
